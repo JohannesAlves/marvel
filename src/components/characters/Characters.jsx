a833490cfdea2567cabcab72e86626dcbeb4b4c2
Johannes <johannes001002@gmail.com>
2022-08-09 11:27:38 -0300

    initial config of cards and adding props

--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -1,11 +1,16 @@
 import Card from './card/Card'
+import './Characters.css'
+
 
 export default function Characters () {
 
     return (
         <div className='body-card'>
-            <h5>Teste</h5>
-            <Card />
+            <article className='card-article'>
+            <Card heroImage="https://images-na.ssl-images-amazon.com/images/I/61JuZe9hWuL.jpg" heroName='Captain Marvel' heroSubtitle='Capitã Marvel, parte do exército de elite dos Kree, uma raça alienígena, encontra-se no meio de uma batalha entre seu povo e os Skrulls.'/>
+            <Card heroImage='https://upload.wikimedia.org/wikipedia/pt/thumb/0/03/Thanos_por_Jim_Starlin.png/260px-Thanos_por_Jim_Starlin.png' heroName='Thanos' heroSubtitle='A lua Titã era governada por Mentor ALars, quando então reinava paz e tecnologia. '/>
+            <Card heroImage='https://geeknfeminist.com.br/wp-content/uploads/2020/03/perfil-hq-tempestade-21.jpg' heroName='Tempestade' heroSubtitle='Ororo Munroe descende de uma antiqüíssima linha de sacerdotisas africanas dotadas de cabelos brancos desde a infância, olhos azuis e grande potencial para feitos,mágicos. A mãe de Ororo era princesa de uma tribo do Quênia que se casou com um jornalista americano.'/>
+            </article>
         </div>
     )
 }
--- src/components/characters/card/Card.jsx
+++ src/components/characters/card/Card.jsx
@@ -1,21 +1,24 @@
 import './card.css'
 
 
-export default function Card () {
+export default function Card (props) {
 
     return (
         <>
+    <div className='container-card'>
         <article class="card">
             <header class="card__thumb">
-                <a href="#"><img src="https://images-na.ssl-images-amazon.com/images/I/61JuZe9hWuL.jpg"/></a>
+                <a href="#"><img src={props.heroImage}/></a>
             </header>
             <div class="card__body">
-                <h2 class="card__title"><a href="#">Captain Marvel</a></h2>
-                <div class="card__subtitle">Capitã Marvel, parte do exército de elite dos Kree, uma raça alienígena, encontra-se no meio de uma batalha entre seu povo e os Skrulls. </div>
-                <p class="card__description">5. Pour the mixture into a non-stick container and then freeze overnight. Take treating your dog a step further by turning it into an ice cream sundae party!</p>
+                <h2 class="card__title"><a href="#">{props.heroName}</a></h2>
+                <div class="card__subtitle"> {props.heroSubtitle} </div>
+                <p class="card__description">{props.heroDescription}</p>
             </div>
-
         </article>
+    </div>
         </>
     )
 }
+//"https://images-na.ssl-images-amazon.com/images/I/61JuZe9hWuL.jpg" 
+//
