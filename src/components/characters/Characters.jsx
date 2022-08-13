3d3cc09293b361bb1b0b01d403001955d30fb2ea
Johannes <johannes001002@gmail.com>
2022-08-12 21:40:08 -0300

    creating loading folders and logic.

--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -1,48 +1,57 @@
+import { useEffect, useState } from 'react';
 import Card from './card/Card'
-import style from './style.module.css'
-import { useEffect } from 'react';
-import md5 from 'md5'
-import { useState } from 'react';
 
+import md5 from 'md5'
+import style from './style.module.css'
 
 
 export default function Characters () {
     const [total, setTotal] = useState(0);
     const [characters, setCharacters] = useState([]);
 
-    useEffect(() => {
+    useEffect( () => {
         const apiKey = '4f81be65ec9847a1f604eb1c0a55d48b';
+        const privateKey = import.meta.env.VITE_PRIVATE_KEY;
         let timestamp = new Date().getTime();
-        let hash = md5(timestamp+privateKey+apiKey)
+        let hash = md5(timestamp+privateKey+apiKey);
 
+        let url = `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hash}&limit=20`
 
-        let url = `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hash}&limit=100`
-
-        let settings = {
-            method: 'GET',
-        }
-
-        fetch(url, settings)
-        .then(response => response.json())
+        fetch(url)
+        .then(response => {
+            return response.json()
+        })
         .then(characters => {
             const {data} = characters
             setTotal(data.total)
             setCharacters(data.results)
         })
         .catch(err => err.json())
+
+
+
     }, [])
+
+    function ContentCharacters () {
+        return (
+        <>
+            <h2 className={style.heroCount}>Total de heróis: {total}</h2>
+            {characters.map(character => {
+                    return <Card heroName={character.name} heroSubtitle={character.description} heroImage={character.thumbnail.path+'.jpg'}/>
+                })}
+        </>
+        )
+    }
+ 
 
 
 
     return (
         <div className='body-card'>
-            <h2>Total de heróis: {total}</h2>
             <article className={style.cardArticle}>
-                {characters.map(character => {
-                    console.log(character.thumbnail.path)
-                    return <Card heroName={character.name} heroSubtitle={character.description} heroImage={character.thumbnail.path+'.jpg'}/>
-                })}
+                <ContentCharacters />
             </article>
         </div>
     )
 }
+
---
+++ src/components/loadingSpinner/Loading.jsx
@@ -0,0 +1,8 @@
+
+function Loading () {
+    return (
+        <h1>Carregando...</h1>
+    )
+}
+
+export default Loading;
